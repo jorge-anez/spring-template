8a40c9c81d05483d9a3c3a8da49bc16323e34222
jorge12387 <jorge.aniez@gmail.com>
2017-03-20 17:43:24 -0400

    primesfaces dependenciecs ready

---
+++ src/main/java/com/project/controller/PrimefacesBean.java
@@ -0,0 +1,42 @@
+package com.project.controller;
+
+import com.project.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.io.Serializable;
+
+
+@ManagedBean
+@RequestScoped
+@Component
+public class PrimefacesBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Autowired
+    private PersonService personService;
+
+    private String firstName;
+    private String lastName;
+    public void save(){
+        System.out.println("save example" + personService);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
